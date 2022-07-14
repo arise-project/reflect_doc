227913d2bb6b32efad2f45c82fc0f7223ce2635e
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-07-14 16:25:34 +0300

    dotnet-format

--- Program.cs
+++ Program.cs
@@ -11,100 +11,100 @@ namespace reflect_doc
 {
     class Program
     {
-    	private static Type GetEntityType(string typeName)
-		{
-			new Process();
-		    List<System.Reflection.Assembly> assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
-		
-		    foreach (var assembly in assemblies)
-		    {
-		        Type t = assembly.GetType(typeName, false);
-		        if (t != null)
-		            return t;
-		    }
-		    
-		    throw new ArgumentException(
-		        "Type " + typeName + " doesn't exist in the current app domain");
-		}
-		
+        private static Type GetEntityType(string typeName)
+        {
+            new Process();
+            List<System.Reflection.Assembly> assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+
+            foreach (var assembly in assemblies)
+            {
+                Type t = assembly.GetType(typeName, false);
+                if (t != null)
+                    return t;
+            }
+
+            throw new ArgumentException(
+                "Type " + typeName + " doesn't exist in the current app domain");
+        }
+
         static void Main(string[] args)
         {
-            Type customerType = GetEntityType(args[0]);
+            Type type = GetEntityType(args[0]);
 
-		var search = args.Length > 1 ?  args[1] : null;
+            var search = args.Length > 1 ? args[1] : null;
 
-			FieldInfo[] fields = customerType.GetFields();
-			if(fields.Length > 0)
-				Console.WriteLine("Fields: ");
-			foreach (FieldInfo fi in fields)
-			{
-				if(search == null || fi.Name.Contains(search))
-				Console.WriteLine("\t" + fi.Name);
-			}
-			
-			ConstructorInfo[] constructors = customerType.GetConstructors();
-			if(constructors.Length > 0)
-				Console.WriteLine("Constructors: ");
-			
-			foreach (ConstructorInfo ci in constructors)
-			{
-				if(search == null || ci.Name.Contains(search))
-				Console.WriteLine("\t" + ci.Name);
-			}
-			
-			MethodInfo[] methods = customerType.GetMethods();
-			if(methods.Length > 0)
-				Console.WriteLine("Methods: ");
-			foreach (MethodInfo mi in methods)
-			{
-				if(search == null || mi.Name.Contains(search))
-				Console.WriteLine("\t" + mi.Name);
-			}
-			
-			Type[] nestedTypes = customerType.GetNestedTypes();
-			if(nestedTypes.Length > 0)
-				Console.WriteLine("Nested types: ");
-			foreach (Type t in nestedTypes)
-			{
-				if(search == null || t.Name.Contains(search))
-				Console.WriteLine("\t" + t.Name);
-			}
-			
-			PropertyInfo[] properties = customerType.GetProperties();
-			if(properties.Length > 0)
-				Console.WriteLine("Properties: ");
-			foreach (PropertyInfo pi in properties)
-			{
-				if(search == null || pi.Name.Contains(search))
-				Console.WriteLine("\t" + pi.Name);
-			}
-			
-			MemberInfo[] members = customerType.GetMembers();
-			if(members.Length > 0)
-				Console.WriteLine("Members type: ");
-			foreach (MemberInfo mi in members)
-			{
-				if(search == null || mi.Name.Contains(search))
-				{
-					Console.WriteLine();
-					Console.WriteLine("\t{0}, name: {1}", mi.Name, mi.MemberType);
-					Console.WriteLine("\t"+mi);
-					try
-					{
-						//var methodDoc = DocsService.GetXmlFromMember(mi);
-						//Console.WriteLine("\t" + methodDoc.ToString());	
-					}
-					catch
-					{
-						
-					}
-				}
-			}
-			
-			File.Exists("test");	
-			
-			// From type. https://github.com/giacomelli/DocsByReflection
-			/*
+            FieldInfo[] fields = type.GetFields();
+            if (fields.Length > 0)
+                Console.WriteLine("Fields: ");
+            foreach (FieldInfo fi in fields)
+            {
+                if (search == null || fi.Name.Contains(search))
+                    Console.WriteLine("\t" + fi.Name);
+            }
+
+            ConstructorInfo[] constructors = type.GetConstructors();
+            if (constructors.Length > 0)
+                Console.WriteLine("Constructors: ");
+
+            foreach (ConstructorInfo ci in constructors)
+            {
+                if (search == null || ci.Name.Contains(search))
+                    Console.WriteLine("\t" + ci.Name);
+            }
+
+            MethodInfo[] methods = type.GetMethods();
+            if (methods.Length > 0)
+                Console.WriteLine("Methods: ");
+            foreach (MethodInfo mi in methods)
+            {
+                if (search == null || mi.Name.Contains(search))
+                    Console.WriteLine("\t" + mi.Name);
+            }
+
+            Type[] nestedTypes = type.GetNestedTypes();
+            if (nestedTypes.Length > 0)
+                Console.WriteLine("Nested types: ");
+            foreach (Type t in nestedTypes)
+            {
+                if (search == null || t.Name.Contains(search))
+                    Console.WriteLine("\t" + t.Name);
+            }
+
+            PropertyInfo[] properties = type.GetProperties();
+            if (properties.Length > 0)
+                Console.WriteLine("Properties: ");
+            foreach (PropertyInfo pi in properties)
+            {
+                if (search == null || pi.Name.Contains(search))
+                    Console.WriteLine("\t" + pi.Name);
+            }
+
+            MemberInfo[] members = type.GetMembers();
+            if (members.Length > 0)
+                Console.WriteLine("Members type: ");
+            foreach (MemberInfo mi in members)
+            {
+                if (search == null || mi.Name.Contains(search))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("\t{0}, name: {1}", mi.Name, mi.MemberType);
+                    Console.WriteLine("\t" + mi);
+                    try
+                    {
+                        //var methodDoc = DocsService.GetXmlFromMember(mi);
+                        //Console.WriteLine("\t" + methodDoc.ToString());	
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+            File.Exists("test");
+
+            // From type. https://github.com/giacomelli/DocsByReflection
+            /*
 			var typeDoc = DocsService.GetXmlFromType(typeof(Stub));
 			
 			// From property.
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # reflect_doc
 build docs from .net metadata for offline programming
+
+
+format code
+
+dotnet tool install --global dotnet-format --version 3.1.37601
+
+dotnet tool uninstall --global dotnet-format
